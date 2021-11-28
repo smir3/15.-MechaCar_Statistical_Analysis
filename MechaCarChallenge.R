4475af45021ab3f0be8f2179d1f075702531e8bb
smir3 <86159728+smir3@users.noreply.github.com>
2021-11-28 14:55:30 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,13 +8,8 @@ coil_data <- read.csv('Suspension_Coil.csv')
 total_summary <- coil_data %>% summarize(mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 # Deliverable 3
-plt <- ggplot(coil_data,aes(x=log10(PSI))) # import dataset into ggplot2
-plt + geom_density()
-sample_table <- coil_data %>% sample_n(50) #randomly sample 50 data points
-plt2 <- ggplot(sample_table,aes(x=log10(PSI))) #import dataset into ggplot2
-plt2 + geom_density() #visualize distribution using density plot
 #T-test
-t.test(sample_table$PSI,mu=1500) #compare sample versus population means
+t.test(coil_data$PSI,mu=1500) #compare sample versus population means
 #Subset 1
 t.test(subset(coil_data,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
 #Subset 2
